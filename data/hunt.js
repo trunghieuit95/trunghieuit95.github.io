f8204b477ebe97ac06e790b061740554f0b8bdcb
hieunt <hieunt.jvb@gmail.com>
2018-03-30 11:53:47 +0700

    truy na mat

--- data/hunt.js
+++ data/hunt.js
@@ -1,123 +1,95 @@
-    <p><strong><img class="alignleft size-full wp-image-4304" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-01.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 01" width="100" height="100"><strong>Tam Vĩ Hồ</strong>
-        <br> Gợi ý: Cây Anh Đào – Màu Đỏ – Hoa Vũ.
-        <br> Đáp án quái ẩn: Tam Vĩ Hồ – Sanbi no Kitsune.
-        <br> Vị trí xuất hiện: Thám hiểm map 6, Rắn tầng 1.</p>
-    <p><strong><img class="alignleft size-full wp-image-4305" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-02.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 02" width="100" height="100">Độc Nhãn Tiểu Tăng</strong>
-        <br> Gợi ý: Mắt, Phật Thạch – Mắt, Sư Thầy – Kim Cang Kinh.
-        <br> Đáp án quái ẩn: Độc Nhãn Tiểu Tăng – Hitotsume Kozou.
-        <br> Vị trí cần tìm: Thám hiểm map 11, Rắn tầng 1.</p>
-    <p><strong><img class="alignleft size-full wp-image-4306" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-03.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 03" width="100" height="100">Tọa Phu Đồng Tử</strong>
-        <br> Gợi ý: Quỷ Hỏa – Sừng.
-        <br> Đáp án quái ẩn: Tọa Phu Đồng Tử – Zashiki Warashi.
-        <br> Vị trí cần tìm: Boss map 2 thám hiểm, Rắn tầng 3.</p>
-    <p><strong><img class="alignleft size-full wp-image-4307" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-04.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 04" width="100" height="100">Quỷ Sứ Bạch</strong>
-        <br> Gợi ý: Minh Giới – Trắng – Đoạt Mệnh.
-        <br> Đáp án quái ẩn: Quỷ Sứ Bạch – Shiro Mujou.
-        <br> Vị trí cần tìm: Thám hiểm map 16, Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4308" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-05.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 05" width="100" height="100">Quỷ Sứ Hắc</strong>
-        <br> Gợi ý: Lưỡi Hái – Đoản Đao – Minh Giới.
-        <br> Đáp án quái ẩn: Quỷ Sứ Hắc – Kuro Mujou.
-        <br> Vị trí cần tìm: Boss map 16 thám hiểm, Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4309" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-06.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 06" width="100" height="100">Khiêu Khiêu Ca Ca</strong>
-        <br> Gợi ý: Nến – Quan Tài.
-        <br> Đáp án quái ẩn: Khiêu Khiêu Ca Ca – Kyonshi Ani.
-        <br> Vị trí cần tìm: Boss map 12 thám hiểm, Rắn tầng 5.</p>
-    <p><strong><img class="alignleft size-full wp-image-4310" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-07.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 07" width="100" height="100">Yêu Hồ</strong>
-        <br> Gợi ý: Thư Sinh – Mặt Nạ.
-        <br> Đáp án quái ẩn: Yêu Hồ – Youko.
-        <br> Vị trí cần tìm: Boss map 7 thám hiểm, Rắn tầng 2.</p>
-    <p><strong><img class="alignleft size-full wp-image-4311" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-08.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 08" width="100" height="100">Cốt Nữ</strong>
-        <br> Gợi ý: Đầu Lâu – Oán Hận.
-        <br> Đáp án quái ẩn: Cốt Nữ – Hone Onna.
-        <br> Vị trí cần tìm: Rắn tầng 5.</p>
-    <p><strong><img class="alignleft wp-image-4313 size-full" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-09.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 09" width="100" height="100">Hà Đồng</strong>
-        <br> Gợi ý: Hồ Nước – Lá Sen.
-        <br> Đáp án quái ẩn: Hà Đồng – Kappa.
-        <br> Vị trí cần tìm: Thám hiểm map 7.</p>
-    <p><strong><img class="alignleft wp-image-4314 size-full" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-10.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 10" width="100" height="100">Hồ Điệp Tinh</strong>
-        <br> Gợi ý: Trống Tay – Khả Ái.
-        <br> Đáp án quái ẩn: Hồ Điệp Tinh – Kochou no Sei.
-        <br> Vị trí cần tìm: Thám hiểm map 6, Rắn tầng 3.</p>
-    <p><strong><img class="alignleft size-full wp-image-4315" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-11.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 11" width="100" height="100">Khôi Lỗi Sư</strong>
-        <br> Gợi ý: Hình Nhân – Thao Túng.
-        <br> Đáp án quái ẩn: Khôi Lỗi Sư – Kairaishi.
-        <br> Vị trí cần tìm: Thám hiểm map 10, Rắn tầng 7.</p>
-    <p><strong><img class="alignleft size-full wp-image-4316" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-12.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 12" width="100" height="100">Hấp Huyết Cơ</strong>
-        <br> Gợi ý: Dơi Máu.
-        <br> Đáp án quái ẩn: Hấp Huyết Cơ – Kyuuketsuhime.
-        <br> Vị trí cần tìm: Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4318" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-13.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 13" width="100" height="100">Tiêu Đồ</strong>
-        <br> Gợi ý: Quạt – Nước – Vỏ Sò – Đuôi.
-        <br> Đáp án quái ẩn: Tiêu Đồ – Shouzu.
-        <br> Vị trí cần tìm: Rắn tầng 3, 8.</p>
-    <p><strong><img class="alignleft size-full wp-image-4319" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-14.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 14" width="100" height="100">Sơn Đồng</strong>
-        <br> Gợi ý: Thạch Chùy – Quái Lực.
-        <br> Đáp án quái ẩn: Sơn Đồng – Yamawaro.
-        <br> Vị trí cần tìm: Thám hiểm map 6, Rắn tầng 1.</p>
-    <p><strong><img class="alignleft size-full wp-image-4320" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-15.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 15" width="100" height="100">Đại Thiên Cẩu</strong>
-        <br> Gợi ý: Lông Vũ – Sáo – Quạt.
-        <br> Đáp án quái ẩn: Đại Thiên Cẩu – Ootengu.
-        <br> Vị trí cần tìm: Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4321" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-16.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 16" width="100" height="100">Liêm Dứu</strong>
-        <br> Gợi ý: Chùy – Đinh Ba – Rìu.
-        <br> Đáp án quái ẩn: Liêm Dứu – Kamaitachi.
-        <br> Vị trí cần tìm: Rắn tầng 5.</p>
-    <p><strong><img class="alignleft size-full wp-image-4322" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-17.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 17" width="100" height="100">Hải Phường Chủ</strong>
-        <br> Gợi ý: Biển – Râu – Trượng.
-        <br> Đáp án quái ẩn: Hải Phường Chủ – Umibouzu.
-        <br> Vị trí cần tìm: Thám hiểm map 12, Rắn tầng 3.</p>
-    <p><strong><img class="alignleft size-full wp-image-4323" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-18.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 18" width="100" height="100">Thực Mộng Mô</strong>
-        <br> Gợi ý: Chuông – Ác Mộng.
-        <br> Đáp án quái ẩn: Thực Mộng Mô – Yumekui.
-        <br> Vị trí cần tìm: Boss map 14 thám hiểm, Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4324" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-19.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 19" width="100" height="100">Đồng Nam</strong>
-        <br> Gợi ý: Cánh – Vũ Y – Hiến Tế
-        <br> Đáp án quái ẩn: Đồng Nam – Oguna.
-        <br> Vị trí cần tìm: Thám hiểm map 12, Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4325" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-20.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 20" width="100" height="100">Đồng Nữ</strong>
-        <br> Gợi ý: Cánh – Vũ Y – Bé Gái Nhỏ.
-        <br> Đáp án quái ẩn: Đồng Nữ – Doujo.
-        <br> Vị trí cần tìm: Thám hiểm map 3, 11, 12, Rắn tầng 2.</p>
-    <p><strong><img class="alignleft size-full wp-image-4326" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-21.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 21" width="100" height="100">Khuyển Thần</strong>
-        <br> Gợi ý: Gâu Gâu – Thủ Hộ.
-        <br> Đáp án quái ẩn: Khuyển Thần – Inugami.
-        <br> Vị trí cần tìm: Thám hiểm map 10, Rắn tầng 4.</p>
-    <p><strong><img class="alignleft size-full wp-image-4327" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-22.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 22" width="100" height="100">Nha Thiên Cẩu</strong>
-        <br> Gợi ý: Naginata – Cánh – Mặt Nạ.
-        <br> Đáp án quái ẩn: Nha Thiên Cẩu – Karasu Tengu.
-        <br> Vị trí cần tìm: Thám hiểm map 3, 9, 12, Rắn tầng 6.</p>
-    <p><strong><img class="alignleft size-full wp-image-4329" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-23.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 23" width="100" height="100">Sửu Thì Chi Nữ</strong>
-        <br> Gợi ý: Người Rơm – Trù Ếm.
-        <br> Đáp án quái ẩn: Sửu Thì Chi Nữ – Ushi no Kakumairi.
-        <br> Vị trí cần tìm: Thám hiểm map 10, Rắn tầng 7.</p>
-    <p><strong><img class="alignleft size-full wp-image-4330" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-24.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 24" width="100" height="100">Vũ Nữ</strong>
-        <br> Gợi ý: Khóc – Mưa – Dù.
-        <br> Đáp án quái ẩn: Vũ Nữ – Ame Onna.
-        <br> Vị trí cần tìm: Boss map 4 thám hiểm, Rắn tầng 6.</p>
-    <p><strong><img class="alignleft size-full wp-image-4331" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-25.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 25" width="100" height="100">Diêm Ma</strong>
-        <br> Gợi ý: Mây – Minh Giới.
-        <br> Đáp án quái ẩn: Diêm Ma – Enma.
-        <br> Vị trí cần tìm: Rắn tầng 6.</p>
-    <p><strong><img class="alignleft size-full wp-image-4332" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-26.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 26" width="100" height="100">Huỳnh Thảo</strong>
-        <br> Gợi ý: Bồ Công Anh – Cắn.
-        <br> Đáp án quái ẩn: Huỳnh Thảo – Hotarugusa.
-        <br> Vị trí cần tìm: Thám hiểm map 17, Rắn tầng 2.</p>
-    <p><strong><img class="alignleft size-full wp-image-4333" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-27.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 27" width="100" height="100">Binh Dũng</strong>
-        <br> Gợi ý: Binh Giáp – Hóa Đá.
-        <br> Đáp án quái ẩn: Binh Dũng – Heiyou.
-        <br> Vị trí cần tìm: Thám hiểm map 3, Rắn tầng 2.</p>
-    <p><strong><img class="alignleft size-full wp-image-4334" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-28.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 28" width="100" height="100">Thiên Tà Quỷ Thanh</strong>
-        <br> Gợi ý: Con Diều.
-        <br> Đáp án quái ẩn: Thiên Tà Quỷ Thanh – Amanojaku Ao.
-        <br> Vị trí cần tìm: Thám hiểm map 6, Rắn tầng 1.</p>
-    <p><strong><img class="alignleft size-full wp-image-4335" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-29.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 29" width="100" height="100">Thanh Oa Từ Khí</strong>
-        <br> Gợi ý: Xúc Xắc – Hũ – Gian Lận.
-        <br> Đáp án quái ẩn: Thanh Oa Từ Khí – Jikigaeru.
-        <br> Vị trí cần tìm: Boss map 4 thám hiểm, Rắn tầng 3.</p>
-    <p><strong><img class="alignleft size-full wp-image-4336" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-30.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 30" width="100" height="100">Đồ Bích<br> </strong>Gợi ý: Tường Đá.
-        <br> Đáp án quái ẩn: Đồ Bích – Norikabe.
-        <br> Vị trí cần tìm: Thám hiểm map 14, Rắn tầng 1.</p>
-    <p><strong><img class="alignleft size-full wp-image-4337" src="https://vinagamemobile.com/wp-content/uploads/2018/02/huong-dan-tim-quai-an-than-bi-31.jpg" alt="Hướng dẫn tìm quái ẩn thần bí 31" width="100" height="100">Mạnh Bà</strong>
-        <br> Gợi ý: Bát – Cầm Nha – Nha
-        <br> Đáp án quái ẩn: Mạnh Bà – Mou Baa.
-        <br> Vị trí cần tìm: Boss map 9 thám hiểm, Rắn tầng 5.</p>
+var data_hunt = [
+        {'hint' : 'Cây Anh Đào – Màu Đỏ – Hoa Vũ.',
+        'answer' : 'Tam Vĩ Hồ – Sanbi no Kitsune.',
+        'locate' : 'Thám hiểm map 6, Rắn tầng 1.'},
+        {'hint' : 'Mắt, Phật Thạch – Mắt, Sư Thầy – Kim Cang Kinh.',
+        'answer' : 'Độc Nhãn Tiểu Tăng – Hitotsume Kozou.',
+        'locate' : 'Thám hiểm map 11, Rắn tầng 1.'},
+        {'hint' : 'Quỷ Hỏa – Sừng.',
+        'answer' : 'Tọa Phu Đồng Tử – Zashiki Warashi.',
+        'locate' : 'Boss map 2 thám hiểm, Rắn tầng 3.'},
+        {'hint' : 'Minh Giới – Trắng – Đoạt Mệnh.',
+        'answer' : 'Quỷ Sứ Bạch – Shiro Mujou.',
+        'locate' : 'Thám hiểm map 16, Rắn tầng 4.'},
+        {'hint' : 'Lưỡi Hái – Đoản Đao – Minh Giới.',
+        'answer' : 'Quỷ Sứ Hắc – Kuro Mujou.',
+        'locate' : 'Boss map 16 thám hiểm, Rắn tầng 4.'},
+        {'hint' : 'Nến – Quan Tài.',
+        'answer' : 'Khiêu Khiêu Ca Ca – Kyonshi Ani.',
+        'locate' : 'Boss map 12 thám hiểm, Rắn tầng 5.'},
+        {'hint' : 'Thư Sinh – Mặt Nạ.',
+        'answer' : 'Yêu Hồ – Youko.',
+        'locate' : 'Boss map 7 thám hiểm, Rắn tầng 2.'},
+        {'hint' : 'Đầu Lâu – Oán Hận.',
+        'answer' : 'Cốt Nữ – Hone Onna.',
+        'locate' : 'Rắn tầng 5.'},
+        {'hint' : 'Hồ Nước – Lá Sen.',
+        'answer' : 'Hà Đồng – Kappa.',
+        'locate' : 'Thám hiểm map 7.'},
+        {'hint' : 'Trống Tay – Khả Ái.',
+        'answer' : 'Hồ Điệp Tinh – Kochou no Sei.',
+        'locate' : 'Thám hiểm map 6, Rắn tầng 3.'},
+        {'hint' : 'Hình Nhân – Thao Túng.',
+        'answer' : 'Khôi Lỗi Sư – Kairaishi.',
+        'locate' : 'Thám hiểm map 10, Rắn tầng 7.'},
+        {'hint' : 'Dơi Máu.',
+        'answer' : 'Hấp Huyết Cơ – Kyuuketsuhime.',
+        'locate' : 'Rắn tầng 4.'},
+        {'hint' : 'Quạt – Nước – Vỏ Sò – Đuôi.',
+        'answer' : 'Tiêu Đồ – Shouzu.',
+        'locate' : 'Rắn tầng 3, 8.'},
+        {'hint' : 'Thạch Chùy – Quái Lực.',
+        'answer' : 'Sơn Đồng – Yamawaro.',
+        'locate' : 'Thám hiểm map 6, Rắn tầng 1.'},
+        {'hint' : 'Lông Vũ – Sáo – Quạt.',
+        'answer' : 'Đại Thiên Cẩu – Ootengu.',
+        'locate' : 'Rắn tầng 4.'},
+        {'hint' : 'Chùy – Đinh Ba – Rìu.',
+        'answer' : 'Liêm Dứu – Kamaitachi.',
+        'locate' : 'Rắn tầng 5.'},
+        {'hint' : 'Biển – Râu – Trượng.',
+        'answer' : 'Hải Phường Chủ – Umibouzu.',
+        'locate' : 'Thám hiểm map 12, Rắn tầng 3.'},
+        {'hint' : 'Chuông – Ác Mộng.',
+        'answer' : 'Thực Mộng Mô – Yumekui.',
+        'locate' : 'Boss map 14 thám hiểm, Rắn tầng 4.'},
+        {'hint' : 'Cánh – Vũ Y – Hiến Tế',
+        'answer' : 'Đồng Nam – Oguna.',
+        'locate' : 'Thám hiểm map 12, Rắn tầng 4.'},
+        {'hint' : 'Cánh – Vũ Y – Bé Gái Nhỏ.',
+        'answer' : 'Đồng Nữ – Doujo.',
+        'locate' : 'Thám hiểm map 3, 11, 12, Rắn tầng 2.'},
+        {'hint' : 'Gâu Gâu – Thủ Hộ.',
+        'answer' : 'Khuyển Thần – Inugami.',
+        'locate' : 'Thám hiểm map 10, Rắn tầng 4.'},
+        {'hint' : 'Naginata – Cánh – Mặt Nạ.',
+        'answer' : 'Nha Thiên Cẩu – Karasu Tengu.',
+        'locate' : 'Thám hiểm map 3, 9, 12, Rắn tầng 6.'},
+        {'hint' : 'Người Rơm – Trù Ếm.',
+        'answer' : 'Sửu Thì Chi Nữ – Ushi no Kakumairi.',
+        'locate' : 'Thám hiểm map 10, Rắn tầng 7.'},
+        {'hint' : 'Khóc – Mưa – Dù.',
+        'answer' : 'Vũ Nữ – Ame Onna.',
+        'locate' : 'Boss map 4 thám hiểm, Rắn tầng 6.'},
+        {'hint' : 'Mây – Minh Giới.',
+        'answer' : 'Diêm Ma – Enma.',
+        'locate' : 'Rắn tầng 6.'},
+        {'hint' : 'Bồ Công Anh – Cắn.',
+        'answer' : 'Huỳnh Thảo – Hotarugusa.',
+        'locate' : 'Thám hiểm map 17, Rắn tầng 2.'},
+        {'hint' : 'Binh Giáp – Hóa Đá.',
+        'answer' : 'Binh Dũng – Heiyou.',
+        'locate' : 'Thám hiểm map 3, Rắn tầng 2.'},
+        {'hint' : 'Con Diều.',
+        'answer' : 'Thiên Tà Quỷ Thanh – Amanojaku Ao.',
+        'locate' : 'Thám hiểm map 6, Rắn tầng 1.'},
+        {'hint' : 'Xúc Xắc – Hũ – Gian Lận.',
+        'answer' : 'Thanh Oa Từ Khí – Jikigaeru.',
+        'locate' : 'Boss map 4 thám hiểm, Rắn tầng 3.'},
+        {'hint' : 'Tường Đá.',
+            'answer' : 'Đồ Bích – Norikabe.',
+        'locate' : 'Thám hiểm map 14, Rắn tầng 1.'},
+        {'hint' : 'Bát – Cầm Nha – Nha',
+        'answer' : 'Mạnh Bà – Mou Baa.',
+        'locate' : 'Boss map 9 thám hiểm, Rắn tầng 5.'}
+        ]
