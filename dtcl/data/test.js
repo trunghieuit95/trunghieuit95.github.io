8db8d16dc9d76f172c10fcc26a084b18ba1e63f5
trunghieuit95 <trunghieuit2906>
2019-06-27 09:43:45 +0700

    update

---
+++ dtcl/data/test.js
@@ -0,0 +1,558 @@
+var data = [
+    {
+        'question': 'Liên Kết Sinh Mệnh của Shouzu, mỗi lần sử dụng hồi phục bao nhiêu % máu?',
+        'answer': '5%'
+    },
+    {
+        'question': 'Cuồng Khiếu của Shuten Douji, mỗi tầng tối đa hồi phục bao nhiêu % máu?',
+        'answer': '12%'
+    },
+    {
+        'question': 'Cờ Cá Chép của Ebisu tối đa kế thừa bao nhiêu % máu của Ebisu?',
+        'answer': '60%'
+    },
+    {
+        'question': 'Câu Hồn Đăng của Aoandon, mỗi lần hấp thu bao nhiêu Quỷ Hỏa địch?',
+        'answer': '1'
+    },
+    {
+        'question': 'Thuật pháp Tiền Là Chính Nghĩa” tiêu hao mấy điểm Quỷ Hỏa?',
+        'answer': '2'
+    },
+    {
+        'question': 'Khiêu chiến phó bản Niên Thú, cần tiêu hao bao nhiêu Sushi?',
+        'answer': '12'
+    },
+    {
+        'question': 'Khiêu chiến phó bản Thủy Quái, cần tiêu hao bao nhiêu Sushi?',
+        'answer': '30'
+    },
+    {
+        'question': 'Trong cốt truyện, Yao Bikuni đợi Seimei bao lâu trong rừng Phượng?',
+        'answer': '30 năm'
+    },
+    {
+        'question': 'Thư Đồng giống thức thần nào dưới đây nhất?',
+        'answer': 'Akajita'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, mắt ai nhiều nhất?',
+        'answer': 'Akajita'
+    },
+    {
+        'question': 'Người dùng tính mạng để phong ấn tạm thời Orochi là ai?',
+        'answer': 'Âm Dương Sư Kyoto'
+    },
+    {
+        'question': 'Trong số yêu quái dưới đây, ai cầm kiếm ngọc (Kendama) trên tay?',
+        'answer': 'Amanojaku Aka'
+    },
+    {
+        'question': 'Trong số yêu quái dưới đây, ai cầm diều trên tay?',
+        'answer': 'Amanojaku Ao'
+    },
+    {
+        'question': 'Trong số yêu quái dưới đây, ai thích gõ trống?',
+        'answer': 'Amanojaku Ki'
+    },
+    {
+        'question': 'Yêu quái đợi phu quân về trên Cầu Gãy là ai?',
+        'answer': 'Ame Onna'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, người thích kể chuyện là?',
+        'answer': 'Aoandon'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai không có mặt nạ?',
+        'answer': 'Bakedanuki'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Võng Thiết có thể kích hoạt hiệu quả gì?',
+        'answer': 'Bỏ qua phòng thủ'
+    },
+    {
+        'question': 'Đạo cụ cần để triệu hồi thức thần mới?',
+        'answer': 'Bùa'
+    },
+    {
+        'question': 'Hàng tuần lần đầu chia sẻ Thư Viện hoặc triệu hồi, phần thưởng là?',
+        'answer': 'Bùa Thần Bí'
+    },
+    {
+        'question': 'Chó cưng của Kyonshi Imouto tên là gì?',
+        'answer': 'Cà Chua'
+    },
+    {
+        'question': 'Nội dung nào dưới đây không thể xem ở Thư Viện?',
+        'answer': 'Cấp Thức thần'
+    },
+    {
+        'question': 'Bộ 2 món của ngự hồn Phá Thế có thể kích hoạt hiệu quả gì?',
+        'answer': 'Chí Mạng +15%'
+    },
+    {
+        'question': 'Bộ 2 món của ngự hồn Tâm Nhãn có thể kích hoạt hiệu quả gì?',
+        'answer': 'Công +15%'
+    },
+    {
+        'question': 'Sở trường của Hakuro là gì?',
+        'answer': 'Cung tên'
+    },
+    {
+        'question': 'Đâu là công việc của Kuro Mujou và Shiro Mujou?',
+        'answer': 'Dẫn hồn'
+    },
+    {
+        'question': 'Đạo cụ có thể cung cấp lượng lớn Exp cho thức thần là?',
+        'answer': 'Daruma'
+    },
+    {
+        'question': 'Thức thần thỏa mãn điều kiện nào dưới đây có thể thăng sao?',
+        'answer': 'Đạt tới cấp cao nhất hiện tại'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Tuyết Hồn có thể kích hoạt hiệu quả gì?',
+        'answer': 'Đóng băng mục tiêu'
+    },
+    {
+        'question': 'Thú cưỡi của Mouba là gì?',
+        'answer': 'Chén'
+    },
+    {
+        'question': 'Đạo cụ có thể đổi trong Shop Bí Kíp có?',
+        'answer': 'Tất cả đều đúng'
+    },
+    {
+        'question': 'Chị gái của Kamigui là ai?',
+        'answer': 'Enenra'
+    },
+    {
+        'question': 'Ân nhân cứu mạng của Hotarugusa là thức thần nào?',
+        'answer': 'Hakuro'
+    },
+    {
+        'question': 'Ân nhân cứu mạng của Hotarugusa là thức thần nào?',
+        'answer': 'Hakuro'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai sinh sống bên Sông Sanzu?',
+        'answer': 'Higanbana'
+    },
+    {
+        'question': 'Âm Dương Sư có thể triệu hồi ảnh phân thân trợ chiến khi chiến đấu là',
+        'answer': 'Hiromasa'
+    },
+    {
+        'question': 'Hiệu quả bộ 4 Ngự Hồn nào dưới đây lần đầu ra trận nhận được Quỷ Hỏa',
+        'answer': 'Hỏa Linh'
+    },
+    {
+        'question': 'Khung-Cách nhận được Nguyệt Kiến là',
+        'answer': 'Hoàn thành thành tựu Phi Châu-Âm Dương Sư'
+    },
+    {
+        'question': 'Sau khi thỏa mãn điều kiện gì thì có thể đặt thức thần vào trong Vườn',
+        'answer': 'Hoàn Thành Truyền Thuyết'
+    },
+    {
+        'question': 'Dưới đây cái nào là thuật pháp của Tesso',
+        'answer': 'Hoành Tài Hộ Thân'
+    },
+    {
+        'question': 'Thông qua cách nào dưới đây có thể nhận được Huy Hiệu',
+        'answer': 'Hội Chiến Săn Bắt'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Niết Bàn có thể kích hoạt hiệu quả gì',
+        'answer': 'Hồi phục HP'
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây có thể tăng thanh hành động của phe mình',
+        'answer': 'Hola Hola'
+    },
+    {
+        'question': 'Trong các thuật pháp sau, thuật pháp nào có thể tăng tốc độ phe mình',
+        'answer': 'Hola Hola'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai không thể nào chữa trị cho đồng đội',
+        'answer': 'Hououka'
+    },
+    {
+        'question': 'Đạo cụ dùng để thăng cấp Hộp Sushi là',
+        'answer': 'Huy Hiệu'
+    },
+    {
+        'question': 'Trong số yêu quái dưới đây, ai được gọi là Thần Gió',
+        'answer': 'Ichimoku Ren'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, người sinh ra từ trong tranh là',
+        'answer': 'Kachou Fuugetsu'
+    },
+    {
+        'question': 'Yao Bikuni uy hiếp ai để yêu cầu Seimei giải trừ phong ấn của Orochi',
+        'answer': 'Kagura'
+    },
+    {
+        'question': 'Trong cốt truyện, yêu quái Enma yêu cầu Seimei trấn áp là ai',
+        'answer': 'Kairaishi'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai muốn vĩnh viễn sống cùng anh trai',
+        'answer': 'Kairaishi'
+    },
+    {
+        'question': 'Trong cốt truyện, yêu quái bắt Kagura tới Núi Hắc Dạ là ai',
+        'answer': 'Kamigui'
+    },
+    {
+        'question': 'Trong cốt truyện, yêu quái nào yêu thầm Koi',
+        'answer': 'Kappa'
+    },
+    {
+        'question': 'Trong cốt truyện, Seimei tìm thứ gì để đánh bại Orochi',
+        'answer': 'Kiếm Kusanagi'
+    },
+    {
+        'question': 'Trong cốt truyện, bạn tốt của Kaguya là ai',
+        'answer': 'Kingyochi'
+    },
+    {
+        'question': 'Hiệu quả bộ 4 Ngự Hồn nào dưới đây là sát thương phản lại phải chịu',
+        'answer': 'Kính Cơ'
+    },
+    {
+        'question': 'Trong cốt truyện, yêu quái nào dụ dỗ Kagura trong mộng cảnh',
+        'answer': 'Kodokushi'
+    },
+    {
+        'question': 'Yêu quái Youko định bắt đi là',
+        'answer': 'Koi'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, có huynh đệ hoặc tỷ muội nào',
+        'answer': 'Kuro Mujou'
+    },
+    {
+        'question': 'Trong cốt truyện, Kurodouji và Shirodouji thành đệ tử của ai',
+        'answer': 'Kuro Mujou và Shiro Mujou'
+    },
+    {
+        'question': 'Trong cốt truyện, là ai giết hại Chim Sẻ bạn thân của Inugami',
+        'answer': 'Kyuuinochi Neko'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai không phải yêu quái dưới nước',
+        'answer': 'Kyuuinochi Neko'
+    },
+    {
+        'question': 'Máu càng thấp, sát thương tấn công thuật pháp càng lớn là của',
+        'answer': 'Kyuuketsuhime'
+    },
+    {
+        'question': 'Susabi là sứ giả của thần, ban đầu giáng lâm ở đâu',
+        'answer': 'Làng bên bờ biển'
+    },
+    {
+        'question': 'Trong cốt truyện, Kingyochi muốn trả thù ai',
+        'answer': 'Lãnh Chúa Arakawa'
+    },
+    {
+        'question': 'Hội Cầu Nguyện có thể nhận được gì',
+        'answer': 'Mảnh Khế Ước'
+    },
+    {
+        'question': 'Tham gia Bách Quỷ Dạ Hành, nhận được gì',
+        'answer': 'Mảnh khế ước'
+    },
+    {
+        'question': 'Bộ 2 món của ngự hồn Thế Hồn có thể kích hoạt hiệu quả gì',
+        'answer': 'Máu +15%'
+    },
+    {
+        'question': 'Trong cốt truyện, mảnh Kiếm Kusanagi giấu ở đâu',
+        'answer': 'Minh Giới'
+    },
+    {
+        'question': 'Seimei Hắc Ám bảo ai ăn thịt người để bảo vệ sắc đẹp',
+        'answer': 'Momiji'
+    },
+    {
+        'question': 'Bạn tốt của Sakura no Sei là ai',
+        'answer': 'Momo no Sei'
+    },
+    {
+        'question': 'Dưới đây thức thần hiếm có nào không thể thức tỉnh',
+        'answer': 'N'
+    },
+    {
+        'question': 'Nơi vào xem ngoại truyện là',
+        'answer': 'Ngoại Truyện'
+    },
+    {
+        'question': 'Thú cưỡi của thức thần MouBa là gì',
+        'answer': 'Nha Nha'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Hỏa Linh có thể kích hoạt hiệu quả gì',
+        'answer': 'Nhận 3 Quỷ Hỏa'
+    },
+    {
+        'question': 'Muốn xem lại các cốt truyện đã mở khóa, nhấn vào',
+        'answer': 'Nhấn vào Cây Anh Đào trong Vườn – Hồi Ức'
+    },
+    {
+        'question': 'Trong các thuật pháp sau, thuật pháp nào có thể câm lặng địch',
+        'answer': 'Oán Hồn Trọng Áp'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai nói tiếng khác với những người khác',
+        'answer': 'Oguna/Momo'
+    },
+    {
+        'question': 'Thức Thần của Seimei Hắc Ám là',
+        'answer': 'Ootengu và Yuki Onna'
+    },
+    {
+        'question': 'Cửa vào Tập Kết Huyễn Cảnh ở đâu',
+        'answer': 'Phố'
+    },
+    {
+        'question': 'Mục nào dưới đây không phải nội dung nhiệm vụ hàng ngày',
+        'answer': 'PVP'
+    },
+    {
+        'question': 'Thông qua cách nào dưới đây có thể nhận được Vinh Dự',
+        'answer': 'PVP'
+    },
+    {
+        'question': 'Động vật nào đứng trên khung Ước Nguyện chỗ Lồng Đèn bên trái Vườn',
+        'answer': 'Quạ'
+    },
+    {
+        'question': 'Địa điểm Yao Bikuni và Seimei lần đầu gặp mặt là',
+        'answer': 'Rừng Phượng'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, người giỏi vũ đạo là',
+        'answer': 'Sakura no Sei'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, số đuôi ai nhiều nhất',
+        'answer': 'Sanbi no Kitsune'
+    },
+    {
+        'question': 'Người dạy Momiji ăn thịt người là ai',
+        'answer': 'Seimei Hắc Ám'
+    },
+    {
+        'question': 'Trong cốt truyện, là ai nói với Seimei mảnh Kiếm Kusanagi ở Minh Giới',
+        'answer': 'Seimei Hắc Ám'
+    },
+    {
+        'question': 'Trong cốt truyện, Yuki Onna và Ootengu theo phe Âm Dương Sư nào',
+        'answer': 'Seimei Hắc Ám'
+    },
+    {
+        'question': 'Bạn thân của Ibaraki Douji là ai',
+        'answer': 'Shuten Douji'
+    },
+    {
+        'question': 'Trong các nhân vật dưới đây, ai không biết thổi sáo',
+        'answer': 'Shuten Douji'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai trên đầu không có sừng',
+        'answer': 'Shutendouji'
+    },
+    {
+        'question': 'Hakuro vào trạng thái Minh Tường sẽ tăng loại thuộc tính nào',
+        'answer': 'ST C.Mạng'
+    },
+    {
+        'question': 'Trong cốt truyện, Kingyochi mời ai tham gia Hội Nữ Tử',
+        'answer': 'Susabi'
+    },
+    {
+        'question': 'Huyễn cảnh do Ảo Cảnh Tinh Thần của Susabi kích hoạt có hiệu ứng nào',
+        'answer': 'Susabi Trợ Chiến'
+    },
+    {
+        'question': 'Thuật pháp nào có thể phong ấn hiệu quả Ngự Hồn của thức thần',
+        'answer': 'Tâm Đố Kỵ'
+    },
+    {
+        'question': 'Hiệu quả Thuật pháp thông linh Tật Phong của Kagura là',
+        'answer': 'Tăng 1 lượt đánh'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Thụ Yêu có thể kích hoạt hiệu quả gì',
+        'answer': 'Tăng chữa trị'
+    },
+    {
+        'question': 'Huyễn cảnh do Long Thủ Ngọc của Kaguya kích hoạt không có hiệu ứng nào',
+        'answer': 'Tăng điểm HP'
+    },
+    {
+        'question': 'Trường cảnh nào dưới đây có thể xuất hiện Người Giấy',
+        'answer': 'Tất cả đều đúng'
+    },
+    {
+        'question': 'Ibaraki Douji mất tay nào',
+        'answer': 'Tay phải'
+    },
+    {
+        'question': 'Thông qua Thuật Pháp, không thể làm được gì',
+        'answer': 'Thăng cấp Thức Thần'
+    },
+    {
+        'question': 'Đạo cụ dùng để Đột Phá Kết Giới là',
+        'answer': 'Thẻ Đột Phá'
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây thuật pháp sát thương quần thể',
+        'answer': 'Thiên Vũ Giáng'
+    },
+    {
+        'question': 'Trong các thuật pháp sau, thuật pháp nào là tấn công quần thể',
+        'answer': 'Thiên Vũ Giáng'
+    },
+    {
+        'question': 'Cái gì thưởng báo danh mỗi ngày không có',
+        'answer': 'Thức Thần Mới'
+    },
+    {
+        'question': 'Bộ 4 món của ngự hồn Mộc Mị có thể kích hoạt hiệu quả gì',
+        'answer': 'Trộm Quỷ Hỏa'
+    },
+    {
+        'question': 'Trong cốt truyện, Kairaishi và anh trai của nàng ta ngủ say ở đâu',
+        'answer': 'Trong sông Sanzu'
+    },
+    {
+        'question': 'Khiêu chiến phó bản Nghiệp Nguyên Hỏa, cần loại đạo cụ nào dưới đây',
+        'answer': 'Vé Tham/Sân/Si'
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây là thuật pháp sát thương đơn thể',
+        'answer': 'Vô Ngã'
+    },
+    {
+        'question': 'Câu cửa miệng của Tesso là gì',
+        'answer': 'Nhà có một con chuột…..'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai ngồi trên lưng Chén Tinh',
+        'answer': 'Mouba'
+    },
+    {
+        'question': 'Yêu quái nào chạy trên đường',
+        'answer': 'Yamausagi và Mouba'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, người không có cánh là',
+        'answer': 'Yamawaro'
+    },
+    {
+        'question': 'Trong cốt truyện, Hououka yêu cầu Seimei dẫn ai đi từ rừng Phượng',
+        'answer': 'Yao Bikuni'
+    },
+    {
+        'question': 'Trong số yêu quái dưới đây, có thể ăn ác mộng là',
+        'answer': 'Yumeikui'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai là cô gái đáng yêu',
+        'answer': 'Zashiki Warashi'
+    },
+    {
+        'question': 'Trong cốt truyện, Kuro Douji và Shiro Douji đã trở thành đệ tử của ai?',
+        'answer': 'Kuro Mujou và Shiro Mujou'
+    },
+    {
+        'question': 'Dùng đạo cụ nào bên dưới, là không thể triệu hồi thức thần SSR',
+        'answer': 'Bùa Xám'
+    },
+    {
+        'question': 'Kỹ năng nào không thể giảm sát thương của đồng đội phải chịu',
+        'answer': ''
+    },
+    {
+        'question': 'Mục nào không hỗ trợ tăng thêm trong các mục sau',
+        'answer': 'Kho Báu Người Giấy'
+    },
+    {
+        'question': 'Nguyên liệu thăng cáp Ngự Hồn không gồm cái nào dưới đây',
+        'answer': 'Khuyết ngọc'
+    },
+    {
+        'question': 'Nguyên liệu thăng cấp thức thần không gồm cái nào dưới đây',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào có thể giải trừ hiệu ứng giảm ích của phe mình',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây có thể bảo vệ đồng đội',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây có thể câm lặng địch',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây có thể cung cấp Quỷ Hỏa cho phe mình',
+        'answer': 'Họa Phúc Tương Sinh'
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây có thể hồi sinh đồng đội',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây là thuật pháp chữa trị',
+        'answer': ''
+    },
+    {
+        'question': 'Thuật pháp nào dưới đây là thuật pháp khiêu khích',
+        'answer': ''
+    },
+    {
+        'question': 'Thuộc tính bộ 2 món của Ngự Hồn nào dưới đây không phải chí mạng',
+        'answer': 'Âm Ma'
+    },
+    {
+        'question': 'Thuộc tính bộ 2 món của Ngự Hồn nào dưới đây không phải kháng hiệu ứng',
+        'answer': ''
+    },
+    {
+        'question': 'Trong các thuật pháp sau, thuật pháp nào có hiệu ứng khống chế',
+        'answer': 'Băng phong bạo tuyết'
+    },
+    {
+        'question': 'Trong các thuật pháp sau, thuật pháp nào có thể bảo vệ đồng đội',
+        'answer': ''
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai là bé trai đáng yêu',
+        'answer': 'Hannya'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai tới từ Minh Giới',
+        'answer': ''
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, ai từng là một hòa thượng',
+        'answer': ''
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, người ít nói là',
+        'answer': 'Kuro Mujou'
+    },
+    {
+        'question': 'Trong số thức thần dưới đây, yêu quái không thể bay là ai',
+        'answer': ''
+    },
+]
