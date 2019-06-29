9061d4442025affc0a82c938bd9a8231e221e4af
trunghieuit95 <trunghieuit2906>
2019-06-29 12:40:35 +0700

    first realse

---
+++ dtcl/data/hero.js
@@ -0,0 +1,402 @@
+var data_hero = [
+        {
+			'name' : 'aatrox',
+			'tier' : '3',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/aatrox.png',
+			'cId' : 1,
+			'sId' : 0
+		},
+		{
+			'name' : 'evelyn',
+			'tier' : '2',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/evelyn.png',
+			'cId' : 0,
+			'sId' : 0
+		},
+		{
+			'name' : 'brand',
+			'tier' : '4',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/brand.png',
+			'cId' : 3,
+			'sId' : 0
+		},
+		{
+			'name' : 'varus',
+			'tier' : '2',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/varus.png',
+			'cId' : 7,
+			'sId' : 0
+		},
+		{
+			'name' : 'elise',
+			'tier' : '2',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/elise.png',
+			'cId' : 8,
+			'sId' : 0
+		},
+		{
+			'name' : 'swain',
+			'tier' : '5',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/swain.png',
+			'cId' : 8,
+			'sId' : [0,5]
+		},
+		{
+			'name' : 'morgana',
+			'tier' : '3',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/morgana.png',
+			'cId' : 9,
+			'sId' : 0
+		},
+		{
+			'name' : 'shyvana',
+			'tier' : '3',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/shyvana.png',
+			'cId' : 8,
+			'sId' : 1
+		},
+		{
+			'name' : 'Aurelion Sol',
+			'tier' : '4',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/aurelionsol.png',
+			'cId' : 9,
+			'sId' : 1
+		},
+		{
+			'name' : 'Yasuo',
+			'tier' : '5',
+			'skill-name' : 'Quỷ Kiếm Darkin',
+			'skill-avatar' : '',
+			'skill-detail' : 'Aatrox tung kiếm và tấn công vào khu vực trước mặt, gây sát thương lên tất cả kẻ địch trong vùng ảnh hưởng.',
+			'specific' : 'Tộc Ác Quỷ',
+			'class' : 'Hệ Kiếm Khách',
+			'avatar' : 'data/img/champion/avatar/yasuo.png',
+			'cId' : 1,
+			'sId' : 2
+		},
+		{
+			'name' : 'Volibear',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/volibear.png',
+			'cId' : 2,
+			'sId' : 3
+		},
+		{
+			'name' : 'Lissandra',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/lissandra.png',
+			'cId' : 3,
+			'sId' : 3
+		},
+		{
+			'name' : 'Anivia',
+			'tier' : '5',
+			'avatar' : 'data/img/champion/avatar/anivia.png',
+			'cId' : 3,
+			'sId' : 3
+		},
+		{
+			'name' : 'Braum',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/braum.png',
+			'cId' : 4,
+			'sId' : 3
+		},
+		{
+			'name' : 'Sejuani',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/sejuani.png',
+			'cId' : 6,
+			'sId' : 3
+		},
+		{
+			'name' : 'Ashe',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/ashe.png',
+			'cId' : 7,
+			'sId' : 3
+		},
+		{
+			'name' : 'Blitcrank',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/blitzcrank.png',
+			'cId' : 3,
+			'sId' : 4
+		},
+		{
+			'name' : 'Katarina',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/katarina.png',
+			'cId' : 0,
+			'sId' : 5
+		},
+		{
+			'name' : 'Draven',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/draven.png',
+			'cId' : 1,
+			'sId' : 5
+		},
+		{
+			'name' : 'Darius',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/draius.png',
+			'cId' : 6,
+			'sId' : 5
+		},
+		{
+			'name' : 'Fiora',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/fiora.png',
+			'cId' : 1,
+			'sId' : 6
+		},
+		{
+			'name' : 'leona',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/leona.png',
+			'cId' : 4,
+			'sId' : 6
+		},
+		{
+			'name' : 'Lucian',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/lucian.png',
+			'cId' : 5,
+			'sId' : 6
+		},
+		{
+			'name' : 'Garen',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/garen.png',
+			'cId' : 6,
+			'sId' : 6
+		},
+		{
+			'name' : 'Kayle',
+			'tier' : '5',
+			'avatar' : 'data/img/champion/avatar/kayle.png',
+			'cId' : 6,
+			'sId' : 6
+		},
+		{
+			'name' : 'Vayne',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/vayne.png',
+			'cId' : 7,
+			'sId' : 6
+		},
+		{
+			'name' : 'Akali',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/akali.png',
+			'cId' : 0,
+			'sId' : 7
+		},
+		{
+			'name' : 'Zed',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/zed.png',
+			'cId' : 0,
+			'sId' : 7
+		},
+		{
+			'name' : 'Shen',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/shen.png',
+			'cId' : 1,
+			'sId' : 7
+		},
+		{
+			'name' : 'kennen',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/kennen.png',
+			'cId' : 3,
+			'sId' : [7,12]
+		},
+		{
+			'name' : 'Pyke',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/pyke.png',
+			'cId' : 0,
+			'sId' : 8
+		},
+		{
+			'name' : 'Gangplank',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/gangplank.png',
+			'cId' : [1,5],
+			'sId' : 8
+		},
+		{
+			'name' : 'Miss Fortune',
+			'tier' : '5',
+			'avatar' : 'data/img/champion/avatar/missfortune.png',
+			'cId' : 5,
+			'sId' : 8
+		},
+		{
+			'name' : 'Graves',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/graves.png',
+			'cId' : 5,
+			'sId' : 8
+		},
+		{
+			'name' : 'Mordekaiser',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/mordekaiser.png',
+			'cId' : 6,
+			'sId' : 9
+		},
+		{
+			'name' : 'Kindred',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/kindred.png',
+			'cId' : 7,
+			'sId' : 9
+		},
+		{
+			'name' : 'kathus',
+			'tier' : '5',
+			'avatar' : 'data/img/champion/avatar/karthus.png',
+			'cId' : 9,
+			'sId' : 9
+		},
+		{
+			'name' : 'Rengar',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/rengar.png',
+			'cId' : 0,
+			'sId' : 10
+		},
+		{
+			'name' : 'Wawick',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/warwick.png',
+			'cId' : 2,
+			'sId' : 10
+		},
+		{
+			'name' : 'Nidalee',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/nidalee.png',
+			'cId' : 8,
+			'sId' : 10
+		},
+		{
+			'name' : 'Gnar',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/gnar.png',
+			'cId' : 8,
+			'sId' : [10,12]
+		},
+		{
+			'name' : 'Ahri',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/ahri.png',
+			'cId' : 9,
+			'sId' : 10
+		},
+		{
+			'name' : 'KhaZix',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/khazix.png',
+			'cId' : 0,
+			'sId' : 11
+		},
+		{
+			'name' : 'Reksai',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/reksai.png',
+			'cId' : 2,
+			'sId' : 11
+		},
+		{
+			'name' : 'Chogath',
+			'tier' : '4',
+			'avatar' : 'data/img/champion/avatar/chogath.png',
+			'cId' : 2,
+			'sId' : 11
+		},
+		{
+			'name' : 'Kassadin',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/kassadin.png',
+			'cId' : 9,
+			'sId' : 11
+		},
+		{
+			'name' : 'Tristana',
+			'tier' : '1',
+			'avatar' : 'data/img/champion/avatar/tristana.png',
+			'cId' : 5,
+			'sId' : 12
+		},
+		{
+			'name' : 'Poppy',
+			'tier' : '3',
+			'avatar' : 'data/img/champion/avatar/poppy.png',
+			'cId' : 6,
+			'sId' : 12
+		},
+		{
+			'name' : 'Lulu',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/lulu.png',
+			'cId' : 9,
+			'sId' : 12
+		},
+		{
+			'name' : 'Veirgar',
+			'tier' : '2',
+			'avatar' : 'data/img/champion/avatar/veigar.png',
+			'cId' : 9,
+			'sId' : 12
+		},
+    ]
